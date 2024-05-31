057dc5d6f00ff344b9534f83a4037f2da8e3ed46
DanielJCC <danijocogollo@gmail.com>
2024-05-31 14:07:04 -0500

    fix CORS configuration

--- src/main/java/com/tecMatch/TecMatch/config/WebConfig.java
+++ src/main/java/com/tecMatch/TecMatch/config/WebConfig.java
@@ -6,18 +6,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer(){
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry){
-//                registry.addMapping("/**")
-//                        .allowedOrigins("*") //"http://localhost:5173","https://techmatch.netlify.app/"
-//                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500","https://techmatch.netlify.app/") //"http://localhost:5173","https://techmatch.netlify.app/"
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+    }
 }
